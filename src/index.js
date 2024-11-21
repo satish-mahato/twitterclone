9f42743fd7b7d2bfe3ab14ea96e5a7b134a6d98b
satish7654 <satishsingh202dd+@gmail.com>
2024-11-21 17:51:30 +0545

    tweet service with hashtag

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const connect = require("./config/database");
+const HashtagRepository = require("./repository/hashtag-repository");
+
 const app = express();
 
 app.listen(3000, async () => {
   console.log("server started");
   await connect();
   console.log("Mongo db connected");
+  let repo = new HashtagRepository();
+  const response = await repo.findByName(["Chair", "computer"]);
+  console.log(response);
 });
--- src/services/tweet-services.js
+++ src/services/tweet-services.js
@@ -1,14 +1,27 @@
-const { TweetRepository } = require("../repository/index");
+const { TweetRepository, HashtagRepository } = require("../repository/index");
 class TweetService {
   constructor() {
     this.tweetRepository = new TweetRepository();
+    this.hashtagRepository = new HashtagRepository();
   }
   async create(data) {
     const content = data.content;
-    const tags = content.match(/#[a-zA-Z0-9_]+/g); // this is regex extracted hashtgs
-    tags = tags.map((tags) => tags.substring(1));
-    console.log(tags);
+    const tags = content
+      .match(/#[a-zA-Z0-9_]+/g)
+      .map((tag) => tag.substring(1));
+
     const tweet = await this.tweetRepository.create(data);
+    const alreadyPresentTags = this.hashtagRepository.findByName(tags);
+    let titleOfPresenttags = alreadyPresentTags.map((tags) => tags.title);
+    let newTags = tags.filter((tag) => !titleOfPresenttags.includes(tag));
+    newTags = newTags.map((tag) => {
+      return { title: tag, tweets: [tweet.id] };
+    });
+    await this.hashtagRepository.bulkCreate(newTags);
+    alreadyPresentTags.forEach((tag) => {
+      tag.tweets.push(tweet.id);
+      tag.save();
+    });
     return tweet;
   }
 }
